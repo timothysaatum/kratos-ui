110e345912a6d55d508d3654ed9bc3b690106551
timothysaatum <tsaatum@outlook.com>
2025-11-14 13:10:05 +0000

    Update: update url versatily

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = 'http://localhost:8000/api';
+// const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:8000/api";
 
 export const api = {
   async request(endpoint, options = {}) {
